ba752a0b8a57524ce0b751a8535d986ee6e5ba4a
AbelBatalla <abelbatallaf@gmail.com>
2023-11-28 21:38:02 +0100

    customizable gun & bullet parameters

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     public Vector3 rotationAxis = Vector3.up; // Define the axis (up, right, forward, etc.)
-    public float rotationSpeed = -180f; // Speed of rotation
+    public float rotationSpeed = 180f; // Speed of rotation
     public float life = 1.5f;
+    public bool rotateRight = true;
+    public float ySpread = 0;
     void Awake()
     {
         Destroy(gameObject, life);
@@ -14,6 +16,16 @@ public class BulletBehaviour : MonoBehaviour
 
     void Update()
     {
-        transform.RotateAround(Vector3.zero, rotationAxis, rotationSpeed * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, rotationAxis, (rotateRight ? -rotationSpeed : rotationSpeed) * Time.deltaTime);
+        transform.Translate(Vector3.up * ySpread * Time.deltaTime);
     }
+
+    public void InitializeBullet(float rotationSpeedValue, bool rotateRightValue, float ySpreadValue, float lifeValue)
+    {
+        rotationSpeed = rotationSpeedValue;
+        rotateRight = rotateRightValue;
+        life = lifeValue;
+        ySpread = ySpreadValue;
+    }
+
 }
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,14 +6,70 @@ public class Gun : MonoBehaviour
 {
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
-    public float bulletSpeed = 10;
 
+    //public float bulletSpeed = 10;
+    public float timeBetweenShooting, spread, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    public bool allowInvoke = true;
+    int bulletsLeft, bulletsShot;
+
+    bool shooting, readyToShoot;
+
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        MyInput();
+    }
+
+    private void MyInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (readyToShoot && shooting && bulletsLeft > 0)
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            //bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            bulletsShot = 0;
+            Shoot();
         }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            bulletsLeft = magazineSize;
+        }
+
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        bulletsLeft--;
+        bulletsShot++;
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        float ySpread = Random.Range(-spread, spread);
+        BulletBehaviour BulletBehaviour = bullet.GetComponent<BulletBehaviour>();
+        if (BulletBehaviour != null)
+        {
+            BulletBehaviour.InitializeBullet(180f, true, ySpread, 1.5f);
+        }
+
+
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0) Invoke("Shoot", timeBetweenShots);
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowInvoke = true;
     }
 }
