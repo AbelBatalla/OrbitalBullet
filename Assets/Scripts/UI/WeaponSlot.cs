cee94782879d0a4d93a0c8175ab6192831a66c6c
AbelBatalla <abelbatallaf@gmail.com>
2023-12-29 13:28:48 +0100

    Ammo logic & UI, Audio, General fixes

---
+++ Assets/Scripts/UI/WeaponSlot.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+
+public class WeaponSlot : MonoBehaviour
+{
+    private bool active;
+
+    public GameObject[] weaponModels;
+    public GameObject selector;
+    public TextMeshProUGUI ammoDisplay;
+    public int currentWeapon;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void EquipWeapon(int weaponIndex)
+    {
+        Debug.Log("RECIEVED");
+        for (int i = 0; i < weaponModels.Length; i++)
+        {
+            if (weaponModels[i] != null)
+            {
+                weaponModels[i].SetActive(i == weaponIndex);
+            }
+        }
+        currentWeapon = weaponIndex;
+    }
+
+    public void activeToggle(bool activeSlot) {
+        active = activeSlot;
+        selector.SetActive(activeSlot);
+        if (ammoDisplay != null)
+        {
+            ToggleBold(activeSlot);
+        }
+    }
+
+    public void setAmmo(int ammo) { 
+        if (ammoDisplay != null)
+        {
+            ammoDisplay.SetText(ammo.ToString());
+        }
+    }
+
+    private void ToggleBold(bool bold)
+    {
+
+        if (bold)
+        {
+            // Apply bold formatting
+            ammoDisplay.text = "<b>" + ammoDisplay.text + "</b>";
+        }
+        else
+        {
+            // Remove bold formatting
+            ammoDisplay.text = ammoDisplay.text.Replace("<b>", "").Replace("</b>", "");
+        }
+    }
+}
