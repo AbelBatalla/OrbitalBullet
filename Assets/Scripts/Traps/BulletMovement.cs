1994be4a0e575cc9f8953e071692f0a8f059d45d
AbelBatalla <abelbatallaf@gmail.com>
2023-12-30 22:48:50 +0100

    minor fixes

--- Assets/Scripts/Traps/BulletMovement.cs
+++ Assets/Scripts/Traps/BulletMovement.cs
@@ -20,7 +20,6 @@ public class BulletMovement : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
-        Debug.Log("Bullet");
         if(other.CompareTag("Player") || other.CompareTag("Level")){
             
             Instantiate(explosionEffect, transform.position, Quaternion.identity);
