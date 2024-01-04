8362531bd0eb361cd5891d418da293bff3b51455
agraciaLpz <arnaugracialopez@gmail.com>
2024-01-04 03:03:19 +0100

    First steps with the boss

---
+++ Assets/Scripts/Enemies/BossController.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+
+    bool awake = false;
+    private LevelCounter level_counter;
+
+    Vector3 startDirection;
+
+    public GameObject Player;
+
+    private Animator anim;
+
+    public float detectionRadius = 30f;
+    float distanceY;
+    float distanceX;
+
+    public float rotationSpeed = 20f;
+    bool rotated = false;
+
+    bool attacking = false;
+
+    float lastTime_attack = 0.0f;
+
+    
+
+    float frontierMoveOrStay = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        level_counter = Player.GetComponent<LevelCounter>();
+        anim = gameObject.GetComponentInChildren<Animator>();
+        startDirection = transform.position - transform.parent.position;
+        startDirection.y = 0.0f;
+        startDirection.x = 95.0f;
+        startDirection.Normalize();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (awake)
+        {
+            CheckDistance();
+            if (Mathf.Abs(distanceX) > frontierMoveOrStay)
+            {
+                float oldDist = distanceX;
+                transform.RotateAround(Vector3.zero, Vector3.up, rotationSpeed * Time.deltaTime);
+                Vector3 enemyPositionXZ = new Vector3(transform.position.x, 0, transform.position.z);
+                Vector3 playerPositionXZ = new Vector3(Player.transform.position.x, 0, Player.transform.position.z);
+                float newDist = Vector3.Distance(enemyPositionXZ, playerPositionXZ);
+                if (Mathf.Abs(oldDist) < Mathf.Abs(newDist))
+                {
+                    Debug.Log("walking");
+                    if(rotated) {
+                        transform.Rotate(Vector3.up, 180f);
+                        rotated = false;
+                    }
+                    transform.RotateAround(Vector3.zero, Vector3.up, -2 * rotationSpeed * Time.deltaTime);
+                } else if(Mathf.Abs(oldDist) > Mathf.Abs(newDist)){
+                    Debug.Log("walking reverse");
+                    if(!rotated) {
+                        transform.Rotate(Vector3.up, 180f);
+                        rotated = true;
+                    }
+                }
+                Debug.Log(Mathf.Abs(newDist));
+                anim.SetFloat("Blend", 0.5f, 0.1f, Time.deltaTime);
+            }
+            else
+            {
+                float value = Random.value;
+                if(lastTime_attack != 0) lastTime_attack += Time.deltaTime;
+                if(lastTime_attack >= 10.0f){
+                    lastTime_attack = 0;
+                    //anim.SetBool("attack", false);
+                    attacking = false;
+                } 
+                else if(lastTime_attack >= 3.0f) anim.SetBool("attack", false);
+                //Debug.Log(lastTime_attack);
+                if(value > 0.5f){
+                    if(!attacking){
+                        anim.SetBool("attack", true);
+                        lastTime_attack = Time.deltaTime;
+                        Debug.Log(lastTime_attack);
+                        attacking = true;
+                    } 
+                }
+                anim.SetFloat("Blend", 0.0f, 0.1f, Time.deltaTime);
+                if (false)
+                {
+                    //canShoot = false;
+                    //if (bullet != null) Instantiate(bullet, shootPlace.position, shootPlace.rotation);
+                    //Invoke("resetShot", 2f);
+                }
+            }
+            // Correct orientation of player
+            // Compute current direction
+            Vector3 currentDirection = transform.position - transform.parent.position;
+            currentDirection.y = 0.0f;
+            currentDirection.Normalize();
+            // Change orientation of player accordingly
+            Quaternion orientation;
+            if ((startDirection - currentDirection).magnitude < 1e-3)
+                orientation = Quaternion.AngleAxis(0.0f, Vector3.up);
+            else if ((startDirection + currentDirection).magnitude < 1e-3)
+                orientation = Quaternion.AngleAxis(180.0f, Vector3.up);
+            else
+                orientation = Quaternion.FromToRotation(startDirection, currentDirection);
+            transform.rotation = orientation;
+        }
+        else
+        {
+            if (level_counter.getLevel() == 5)
+            {
+                CheckDistance();
+                if (distanceX <= detectionRadius)
+                {
+                    awake = true;
+                }
+            }
+        }
+        
+    }
+
+private void CheckDistance()
+    {
+        Vector3 enemyPositionXZ = new Vector3(transform.position.x, 0, transform.position.z);
+        Vector3 playerPositionXZ = new Vector3(Player.transform.position.x, 0, Player.transform.position.z);
+
+        distanceX = Vector3.Distance(enemyPositionXZ, playerPositionXZ);
+        distanceY = Mathf.Abs(transform.position.y - Player.transform.position.y);
+    }
+    
+}
